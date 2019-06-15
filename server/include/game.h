d2c9f790caf773deab3e3e27a61e1982f90ae62d
siverthreadk <eunsill91@gmail.com>
2019-06-15 20:57:41 +0900

    Add ability to assign mafia randomly #10

--- server/include/game.h
+++ server/include/game.h
@@ -3,19 +3,35 @@
 
 #include <memory>
 #include <string>
+#include <vector>
 
+class Player;
 class Group;
 
 class Game {
 private:
-    std::unique_ptr<Group> players_;    //플레이어 그룹
-    std::unique_ptr<Group> mafia_;      //마피아 그룹
-    std::unique_ptr<Group> citizen_;    //시민 그룹
+    std::vector<std::shared_ptr<Player> > player_info_; //플레이어 정보 
+    std::unique_ptr<Group> players_;                    //플레이어 그룹
+    std::unique_ptr<Group> mafia_;                      //마피아 그룹
+    std::unique_ptr<Group> citizen_;                    //시민 그룹
+
+    int number_of_mafia_;                               //마피아의 수
 
 public:
     Game();
 
     ~Game();
+
+    /*
+     * 게임을 시작한다.
+     */
+    void play();
+
+private:
+    /*
+     * 플레이어에게 역할을 할당한다.
+     */
+    void assignRoles();
 };
 
 #endif
--- server/src/game.cpp
+++ server/src/game.cpp
@@ -1,5 +1,10 @@
 #include "game.h"
 #include "group.h"
+#include "player.h"
+
+#include <algorithm>
+#include <memory>
+#include <random>
 
 Game::Game()
 {
@@ -10,4 +15,32 @@ Game::Game()
 
 Game::~Game()
 {
+}
+
+void Game::play()
+{
+    assignRoles();
+}
+
+void Game::assignRoles()
+{
+    std::string mafia_list = "mafia list : ";
+
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(player_info_.begin(), player_info_.end(), g);
+
+    for (int i = 0; i < number_of_mafia_; ++i) {
+        mafia_->addObserver(player_info_[i]);
+        players_->addObserver(player_info_[i]);
+        mafia_list += (player_info_[i]->getName() + ((i == number_of_mafia_ - 1) ? std::string("") : std::string(", ")));
+    }
+    for (int i = number_of_mafia_; i < player_info_.size(); ++i) {
+        citizen_->addObserver(player_info_[i]);
+        players_->addObserver(player_info_[i]);
+    }
+
+    mafia_->notifyObservers(std::string("You are a mafia."));
+    mafia_->notifyObservers(mafia_list);
+    citizen_->notifyObservers(std::string("You are a citizen."));
 }
